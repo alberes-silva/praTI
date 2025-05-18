96de7c8a411ca3e7ac4cf43407f0b3fe251f27ae
Alberes Silva <alberessilva@yahoo.com.br>
2025-05-18 13:39:40 -0300

    Update atividade05.js

--- Lista 01 - PraTI/atividade05.js
+++ Lista 01 - PraTI/atividade05.js
@@ -26,18 +26,18 @@ if(!isNaN(peso)){
 if(imc <= 18.5){
     console.log("Abaixo do peso");
 }
-if(imc >= 18.5 && imc < 24.9){
+else if(imc >= 18.5 && imc < 24.9){
     console.log("Peso normal");
 }
-if(imc >= 25 && imc < 29.9){
+else if(imc >= 25 && imc < 29.9){
     console.log("Sobrepeso");
 }
-if(imc >= 30 && imc < 34.9){
+else if(imc >= 30 && imc < 34.9){
     console.log("Obesidade grau I");
 }
-if(imc >= 35 && imc < 39.9){
+else if(imc >= 35 && imc < 39.9){
     console.log("Obesidade grau II");
 }
-if(imc >= 40) {
+else if(imc >= 40) {
     console.log("Obesidade grau III");
 }
