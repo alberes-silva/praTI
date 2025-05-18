dac19a584980ae14eaebce7a6f65ad9b1a61b098
Alberes Silva <alberessilva@yahoo.com.br>
2025-05-18 16:25:55 -0300

    Update atividade13.js

--- Lista 01 - PraTI/atividade13.js
+++ Lista 01 - PraTI/atividade13.js
@@ -6,9 +6,26 @@
 //Para uso do node no VsCode foi utilizado o comando npm install prompt-sync para obtenção dos dados externos
 const prompt = require('prompt-sync')();
 
+//Criação das variáveis de controle
+let soma = 0;
+let quantidade = 0;
+let numero;
 
-let opcao = Number(prompt(`Para sair digite 0 (zero): `))
 
-while(opcao != 0){
-    console.log("Teste");
-}
+//Procesamento de dados utilizando do-while para controle de encerramento com 0 (zero)
+do {
+  numero = parseFloat(prompt("Digite um valor decimal (0 para encerrar): "));
+
+  if (numero !== 0) {
+    soma += numero;
+    quantidade++;
+  }
+
+} while (numero !== 0);
+
+    if (quantidade > 0) {
+    let media = soma / quantidade;
+    console.log("A média aritmética é: " + media.toFixed(2));
+    } else {
+    console.log("Nenhum número foi inserido para calcular a média.");
+    }
