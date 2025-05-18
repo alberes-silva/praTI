95778c781fecc087eefa47e677d7e92459a6387b
Alberes Silva <alberessilva@yahoo.com.br>
2025-05-18 11:02:11 -0300

    Update atividade03.js
    Atualizações das medidas de parseFloat para Number e atualização para !isNan e  media de 0 até 10

--- Lista 01 - PraTI/atividade03.js
+++ Lista 01 - PraTI/atividade03.js
@@ -1,25 +1,44 @@
+
+/*
+ * Implemente um programa que recebe uma nota de 0 a 10 e classifica como
+ * "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+ */
+
+//Para uso do node no VsCode foi utilizado o comando npm install prompt-sync para obtenção dos dados externos
+
 const prompt = require('prompt-sync')()
 
 //Entrada de dados
-let nota01 = parseFloat(prompt("Digite a primeira nota: "));
-let nota02 = parseFloat(prompt("Digite a segunda nota: "));
-let nota03 = parseFloat(prompt("Digite a terceira nota: "));
-let nota04 = parseFloat(prompt("Digite a quarta nota: "));
+let nota01 = Number(prompt("Digite a primeira nota: "));
+let nota02 = Number(prompt("Digite a segunda nota: "));
+let nota03 = Number(prompt("Digite a terceira nota: "));
+let nota04 = Number(prompt("Digite a quarta nota: "));
 
 //Limpeza do console
 console.clear();
 
 //Calculo da média
+
 let media = (nota01 + nota02 + nota03 + nota04) / 4;
 
+
 //Saída de dados
-console.log("A sua média foi ",media);
+if(parseFloat(media)){
+    console.log("A sua média foi ",media.toFixed(2));
+} else {
+    console.log("Valores inválidos");
+}
+
 
 //Avaliação do resultado da média com saída de dados
-if (media < 5){
+if(!isNaN(media) && media >= 0 && media <=10){
+    if (media < 5){
     console.log("Aluno reprovado");
 } else if (media >= 5 && media < 7){
     console.log("Aluno em recuperação");
 } else {
     console.log("aluno Aprovado");
+}
+} else {
+    console.log("Reveja os valores informados nas notas")
 }
